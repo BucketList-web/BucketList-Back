f52ea6be9a563b2ba84b1ea180bba169cc733c15
Bae-Ji-Won <qowhxk@naver.com>
2022-04-30 23:55:21 +0900

    Service(Controller와 직접적인 연결 부분)

---
+++ src/main/java/spring/basic/demo/service/MemberService.java
@@ -0,0 +1,26 @@
+package spring.basic.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.basic.demo.domain.Member;
+import spring.basic.demo.repository.MemberRepositoryInterface;
+
+@Service
+public class MemberService {
+
+    private MemberRepositoryInterface repository;
+
+    @Autowired
+    public MemberService(MemberRepositoryInterface repository){         // service에서 repository 연결하는 느낌
+        this.repository = repository;                               // 사용자는 repository에 접근 하지 못하고 service까지에만 접근이 
+                                                                    // 가능하도록 구현하기 위하여 service에서 repository를 한번더 감싼 느낌
+    }
+
+    public void join(Member m){                 // join == (repository)saveMember
+        repository.saveMember(m);
+    }
+
+    public Member findMemberBuId(int id){       // findMemberBuId == (repository)findById
+        return repository.findById(id);
+    }
+}
