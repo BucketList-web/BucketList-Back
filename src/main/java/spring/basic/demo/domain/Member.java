c21b192387ee5b9f3f3915c5a73df44677f6da27
Bae-Ji-Won <qowhxk@naver.com>
2022-05-04 21:58:12 +0900

    DTO

--- src/main/java/spring/basic/demo/domain/Member.java
+++ src/main/java/spring/basic/demo/domain/Member.java
@@ -4,22 +4,62 @@ public class Member {
 
     private int id;
     private String name;
+    private String location;
+    private String menu;
+    private int price;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
-
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", menu='" + menu + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
