9cb2f19b7722762a623c7cc4b4740fca78473f7b
Bae-Ji-Won <qowhxk@naver.com>
2022-05-04 21:58:02 +0900

    Controller

--- src/main/java/spring/basic/demo/controller/MemberController.java
+++ src/main/java/spring/basic/demo/controller/MemberController.java
@@ -10,27 +10,31 @@ import spring.basic.demo.domain.Member;
 import spring.basic.demo.service.MemberService;
 
 
-// @Controller      spring bean 사용하여 따로 설정했으므로 삭제해야함
+@Controller      //spring bean 사용하여 따로 설정했으므로 삭제해야함
 public class MemberController {
 
     MemberService service;
     @Autowired
     public MemberController(MemberService service){         // controller와 service 연결하는 느낌
+
         this.service = service;
     }
 
     @GetMapping("members/new")
     public String createMember(){
+
         return "members/createForm";
     }
 
     // URL 이 변경되지 않은 상태에서 실행
     @PostMapping("members/new")
-    public String create(@RequestParam("name") String name){
+    public String create(@RequestParam("name") String name,@RequestParam("location") String location,@RequestParam("menu") String menu,@RequestParam("price") int price ){
 
         Member m = new Member();
         m.setName(name);
-
+        m.setLocation(location);
+        m.setMenu(menu);
+        m.setPrice(price);
         //DB에 입력한 값을 넣어야 해요.
         service.join(m);
 
@@ -50,6 +54,16 @@ public class MemberController {
 
         // 찾은 객체를 통재로 다음 페이지로 넘김
         model.addAttribute("member",m);  // member라는 상자에 m객체를 넣어서 보내줌
+
+        // 다음 페이지로 이동
+        return "members/findMember";
+    }
+
+    @PostMapping("members/allfind")
+    public String allfind(Member model){
+        // Service를 통해서 id로 member를 찾아서
+
+        Member m = service.findall(model);
 
         // 다음 페이지로 이동
         return "members/findMember";
