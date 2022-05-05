c8d6497fd6b766d7c394dfc46eda776ffcf2d4db
Bae-Ji-Won <qowhxk@naver.com>
2022-05-05 16:01:04 +0900

    repository(추상화) - 전체 출력 추가

--- src/main/java/spring/basic/demo/repository/MemberRepositoryInterface.java
+++ src/main/java/spring/basic/demo/repository/MemberRepositoryInterface.java
@@ -2,8 +2,11 @@ package spring.basic.demo.repository;
 
 import spring.basic.demo.domain.Member;
 
+import java.util.List;
+
 public interface MemberRepositoryInterface {
 
     void saveMember(Member m);
     Member findById(int id);
+    List<Member> findAll();
 }
