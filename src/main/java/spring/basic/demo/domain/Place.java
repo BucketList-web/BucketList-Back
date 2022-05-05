7f1bb469f3383f8ac916c248137747793be91aa3
Bae-Ji-Won <qowhxk@naver.com>
2022-05-05 22:34:30 +0900

    장소 DTO(GETTER/SETTER)

---
+++ src/main/java/spring/basic/demo/domain/Place.java
@@ -0,0 +1,49 @@
+package spring.basic.demo.domain;
+
+public class Place {
+    private int id;
+    private String location;
+    private String name;
+    private String content;
+    private String tag;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
