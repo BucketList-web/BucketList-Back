c1eb98ed66d085b8ed721e3357a822177770b370
Bae-Ji-Won <qowhxk@naver.com>
2022-05-05 17:47:40 +0900

    Repository(맛집)

--- src/main/java/spring/basic/demo/repository/JdbcStoreRepository.java
+++ src/main/java/spring/basic/demo/repository/JdbcStoreRepository.java
@@ -1,8 +1,7 @@
 package spring.basic.demo.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import spring.basic.demo.domain.Member;
+import spring.basic.demo.domain.Store;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -11,23 +10,23 @@ import java.util.List;
 
 // h2 DB연결 부분
 // @Repository     spring bean 사용하여 따로 설정했으므로 삭제해야함
-public class JdbcMemberRepository implements MemberRepositoryInterface{
+public class JdbcStoreRepository implements StoreRepositoryInterface {
 
     private DataSource dataSource;
     public int index = 0;
 
     @Autowired
-    public JdbcMemberRepository(DataSource dataSource){     // spring에서 자체적으로 DB에 저장한 값 연결하는 부분
+    public JdbcStoreRepository(DataSource dataSource){     // spring에서 자체적으로 DB에 저장한 값 연결하는 부분
         this.dataSource = dataSource;
     }
 
     @Override
-    public void saveMember(Member m) {
+    public void savedata(Store m) {
         // 객체에 id값 주고
         // DB에 저장
 
-        String sql = "INSERT INTO store(id, name,location,menu,price) values(?,?,?,?,?)";      // id,name열에 값을 넣겠다 ,values(?,?) = 값을 넣겠다
-        //String sql = "INSERT INTO member(name) VALUES(?)";   // identity /id값은 sql에서 알아서 해줌
+        String sql = "INSERT INTO store(id, name,location,menu,price,tag) values(?,?,?,?,?,?)";      // id,name열에 값을 넣겠다 ,values(?,?) = 값을 넣겠다
+
 
         Connection conn = null;
         PreparedStatement pstmt = null;
@@ -35,7 +34,7 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
         try {
             conn = dataSource.getConnection();
             pstmt = conn.prepareStatement(sql);
-            //pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);   // identity  id값을 sql에서 자동으로 해주기 위함
+
 
              m.setId(index++);               // setId 설정       현재 게시글 순서를 맞추기 위해 기본키 값을 설정하는 곳으로 로그인 완성되면
                                             //                    아이디를 키값으로 받아올거기 때문에 아이디로 변경해주면 됨.
@@ -45,7 +44,7 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
             pstmt.setString(3,m.getLocation());
             pstmt.setString(4,m.getMenu());
             pstmt.setInt(5,m.getPrice());
-            //pstmt.setString(1,m.getName());
+            pstmt.setString(6,m.getTag());
 
             pstmt.executeUpdate();
 
@@ -57,7 +56,7 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
     }
 
     @Override
-    public Member findById(int id) {
+    public Store findById(int id) {
 
         String sql = "SELECT * FROM store WHERE id=?";
 
@@ -75,12 +74,13 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
             rs = pstmt.executeQuery();  // select 문 사용시에는 executeQuery 사용
 
             if(rs.next()){          // rs객체에 값이 없다면
-                Member m = new Member();            // member클래스 불러옴
+                Store m = new Store();            // member클래스 불러옴
                 m.setId(rs.getInt("id"));   // m객체의 id값 저장
                 m.setName(rs.getString("name"));    // m객체의 name값 저장
                 m.setLocation(rs.getString("location"));
                 m.setMenu(rs.getString("menu"));
                 m.setPrice(rs.getInt("price"));
+                m.setTag(rs.getString("tag"));
 
                 return m;
             }
@@ -95,8 +95,8 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
     }
 
     @Override
-    public List<Member> findAll() {
-        List<Member> dataList = new ArrayList<>();    // select *from store 결과 담을 리스트
+    public List<Store> findAll() {
+        List<Store> dataList = new ArrayList<>();    // select *from store 결과 담을 리스트
 
         String sql = "SELECT * FROM store";
 
@@ -112,13 +112,13 @@ public class JdbcMemberRepository implements MemberRepositoryInterface{
             rs = pstmt.executeQuery();  // select 문 사용시에는 executeQuery 사용
 
             while(rs.next()){          // rs객체에 결과값이 있다면(모든 데이터를 출력하기 위해 while 사용)
-                Member m = new Member();            // member클래스 불러옴
+                Store m = new Store();            // member클래스 불러옴
                 m.setId(rs.getInt("id"));   // m객체의 id값 저장
                 m.setName(rs.getString("name"));    // m객체의 name값 저장
                 m.setLocation(rs.getString("location"));
                 m.setMenu(rs.getString("menu"));
                 m.setPrice(rs.getInt("price"));
-
+                m.setTag(rs.getString("tag"));
 
                 dataList.add(m);           // dataList에 추가
             }
