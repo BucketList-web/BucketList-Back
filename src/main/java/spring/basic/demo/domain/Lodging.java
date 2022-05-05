4b7cd6b92e4a76756d58effc44d85de37afd1a55
Bae-Ji-Won <qowhxk@naver.com>
2022-05-05 21:34:43 +0900

    숙소 DTO(GETTER/SETTER)

---
+++ src/main/java/spring/basic/demo/domain/Lodging.java
@@ -0,0 +1,58 @@
+package spring.basic.demo.domain;
+
+public class Lodging {
+    private int id;
+    private String location;
+    private String name;
+    private int price;
+    private String content;
+    private String tag;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
