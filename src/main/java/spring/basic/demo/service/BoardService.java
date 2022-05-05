7d0660096b293b1327b69285c8e7833cafeb20a7
Bae-Ji-Won <qowhxk@naver.com>
2022-05-05 17:46:09 +0900

    게시글 Service(맛집 완료, 숙소/관광지 작업중)

--- src/main/java/spring/basic/demo/service/BoardService.java
+++ src/main/java/spring/basic/demo/service/BoardService.java
@@ -1,36 +1,39 @@
 package spring.basic.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import spring.basic.demo.domain.Member;
-import spring.basic.demo.repository.MemberRepositoryInterface;
+import spring.basic.demo.domain.Store;
+import spring.basic.demo.repository.StoreRepositoryInterface;
 
 import java.util.List;
 
 // @Service     spring bean 사용하여 따로 설정했으므로 삭제해야함
-public class MemberService {
+public class BoardService {
 
-    private MemberRepositoryInterface repository;
+    // ---------------- 맛집관련 서비스 -----------------------
+    private StoreRepositoryInterface repository;
 
     @Autowired
-    public MemberService(MemberRepositoryInterface repository){         // service에서 repository 연결하는 느낌
+    public BoardService(StoreRepositoryInterface repository){         // service에서 repository 연결하는 느낌
         this.repository = repository;                               // 사용자는 repository에 접근 하지 못하고 service까지에만 접근이
                                                                     // 가능하도록 구현하기 위하여 service에서 repository를 한번더 감싼 느낌
     }
 
-    public void join(Member m){                 // join == (repository)saveMember
+    public void join(Store m){                 // join == (repository)saveMember
 
-        repository.saveMember(m);
+        repository.savedata(m);
     }
 
-    public Member findMemberBuId(int id){       // findMemberBuId == (repository)findById
+    public Store findMemberBuId(int id){       // findMemberBuId == (repository)findById
 
         return repository.findById(id);
     }
 
-    public List<Member> findAll(){
+    public List<Store> findAll(){
 
         return repository.findAll();            // repository에 findAll 함수 호출
     }
+    
+    // ---------------- 숙소관련 서비스 -----------------------
+    
+    
 }
