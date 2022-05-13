f25c6821b4c7c32f4449b66db00dc2bcec43f7db
Bae-Ji-Won <qowhxk@naver.com>
2022-05-13 21:10:05 +0900

    모든 게시물 정보 DTO

---
+++ src/main/java/spring/basic/demo/domain/Data.java
@@ -0,0 +1,58 @@
+//package spring.basic.demo.domain;
+//
+//public class Data {
+//    private int id;
+//    private String name;
+//    private String location;
+//    private String menu;
+//    private int price;
+//    private String tag;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//    public String getMenu() {
+//        return menu;
+//    }
+//
+//    public void setMenu(String menu) {
+//        this.menu = menu;
+//    }
+//
+//    public int getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(int price) {
+//        this.price = price;
+//    }
+//
+//    public String getTag() {
+//        return tag;
+//    }
+//
+//    public void setTag(String tag) {
+//        this.tag = tag;
+//    }
+//}
