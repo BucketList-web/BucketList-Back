ecf45cd46b2fbeb2deedee4573d03ef514f13aab
Bae-Ji-Won <qowhxk@naver.com>
2022-05-13 21:05:00 +0900

    RepositoryInterface - 추상화 메소드 모음

--- src/main/java/spring/basic/demo/repository/BoardRepositoryInterface.java
+++ src/main/java/spring/basic/demo/repository/BoardRepositoryInterface.java
@@ -1,5 +1,6 @@
 package spring.basic.demo.repository;
 
+//import spring.basic.demo.domain.Data;
 import spring.basic.demo.domain.Lodging;
 import spring.basic.demo.domain.Place;
 import spring.basic.demo.domain.Store;
@@ -12,11 +13,14 @@ public interface BoardRepositoryInterface { // service로 함수 보냄
     Store findstoreById(int id);
     List<Store> findstoreAll();
 
-    void savelodging(Lodging m);
-    Lodging findlodgingById(int id);
-    List<Lodging> findlodgingAll();
+//    void savelodging(Lodging m);
+//    Lodging findlodgingById(int id);
+//    List<Lodging> findlodgingAll();
+//
+//    void saveplace(Place m);
+//    Place findplaceById(int id);
+//    List<Place> findplaceAll();
 
-    void saveplace(Place m);
-    Place findplaceById(int id);
-    List<Place> findplaceAll();
+
+//    List<Data> findDataAll();   // 모든 데이터 출력
 }
