0ccd1f754320ba0eb016bb2bce53fe4a4f8830b8
Bae-Ji-Won <qowhxk@naver.com>
2022-05-14 03:44:54 +0900

    RepositoryInterface - 추상화 메소드 저장소

--- src/main/java/spring/basic/demo/repository/BoardRepositoryInterface.java
+++ src/main/java/spring/basic/demo/repository/BoardRepositoryInterface.java
@@ -1,25 +1,15 @@
 package spring.basic.demo.repository;
 
 //import spring.basic.demo.domain.Data;
-import spring.basic.demo.domain.Lodging;
-import spring.basic.demo.domain.Place;
-import spring.basic.demo.domain.Store;
+import spring.basic.demo.domain.Community;
 
 import java.util.List;
 
 public interface BoardRepositoryInterface { // service로 함수 보냄
 
-    void savestore(Store m);
-    Store findstoreById(int id);
-    List<Store> findstoreAll();
-
-//    void savelodging(Lodging m);
-//    Lodging findlodgingById(int id);
-//    List<Lodging> findlodgingAll();
-//
-//    void saveplace(Place m);
-//    Place findplaceById(int id);
-//    List<Place> findplaceAll();
+    void writecommunity(Community m);
+    Community findcommunityById(int id);
+    List<Community> findcommunityAll();
 
 
 //    List<Data> findDataAll();   // 모든 데이터 출력
