25b22c0ae72720453723ff5beb4d651d41d19765
Bae-Ji-Won <qowhxk@naver.com>
2022-05-17 19:43:09 +0900

    로그인 DTO(수정중)

---
+++ src/main/java/spring/basic/demo/domain/Login.java
@@ -0,0 +1,12 @@
+package spring.basic.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class Login {
+    private String Loginid;
+    private String User_id;
+    private String User_pw;
+
+
+}
