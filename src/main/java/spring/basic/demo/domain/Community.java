68d4cd0c9515eea5c551737cdf42daa7fa70e61a
Bae-Ji-Won <qowhxk@naver.com>
2022-05-17 19:37:39 +0900

    커뮤니티 - DTO

--- src/main/java/spring/basic/demo/domain/Community.java
+++ src/main/java/spring/basic/demo/domain/Community.java
@@ -4,8 +4,9 @@ package spring.basic.demo.domain;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
-
+@Data
 public class Community {
 
     private int communityid;            // 게시물 번호 (기본키값)
@@ -14,52 +15,6 @@ public class Community {
     private String communitylocation;   // 위치,장소
     private String communitycontent;     // 내용
     private String communityprice;      // 비용
+    private Date communitydate = new Date(System.currentTimeMillis());
 
-    public int getCommunityid() {
-        return communityid;
-    }
-
-    public void setCommunityid(int communityid) {
-        this.communityid = communityid;
-    }
-
-    public String getCommunitytag() {
-        return communitytag;
-    }
-
-    public void setCommunitytag(String communitytag) {
-        this.communitytag = communitytag;
-    }
-
-    public String getCommunitykind() {
-        return communitykind;
-    }
-
-    public void setCommunitykind(String communitykind) {
-        this.communitykind = communitykind;
-    }
-
-    public String getCommunitylocation() {
-        return communitylocation;
-    }
-
-    public void setCommunitylocation(String communitylocation) {
-        this.communitylocation = communitylocation;
-    }
-
-    public String getCommunitycontent() {
-        return communitycontent;
-    }
-
-    public void setCommunitycontent(String communitycontent) {
-        this.communitycontent = communitycontent;
-    }
-
-    public String getCommunityprice() {
-        return communityprice;
-    }
-
-    public void setCommunityprice(String communityprice) {
-        this.communityprice = communityprice;
-    }
 }
