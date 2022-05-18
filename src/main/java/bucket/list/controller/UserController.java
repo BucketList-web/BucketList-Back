91ac87a8b5f4cd0940aad914c196d3e8776173f1
Daeyeon Ko <dyko615@naver.com>
2022-05-18 22:06:33 +0900

    유저 비밀번호 및 유저회원가입 controller입니다

--- src/main/java/bucket/list/controller/UserController.java
+++ src/main/java/bucket/list/controller/UserController.java
@@ -1,12 +1,30 @@
 package bucket.list.controller;
 
+import bucket.list.domain.User;
+import bucket.list.service.UserService;
+import bucket.list.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
-public class LoginController {
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
 
     @GetMapping("/login")
     public String Login(){
@@ -14,8 +32,30 @@ public class LoginController {
     }
 
     @GetMapping("/create")
-    public String create(){
+    public String createForm(Model model){
+
+        model.addAttribute("user", new User());
 
         return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute("user") User user, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "/user/create";
+        }
+
+        userService.save(user);
+
+        return "/user/create_success";
+    }
+
+    //비밀번호 일치여부 initbinder
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        UserValidator userValidator = new UserValidator();
+
+        webDataBinder.addValidators(userValidator);
     }
 }
