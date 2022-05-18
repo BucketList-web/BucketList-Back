ee53ed1f1f6813e40e60186fbaf156c2199dcfa2
Daeyeon Ko <dyko615@naver.com>
2022-05-18 22:07:35 +0900

    유저서비스입니다

---
+++ src/main/java/bucket/list/service/UserService.java
@@ -0,0 +1,23 @@
+package bucket.list.service;
+
+import bucket.list.domain.User;
+import bucket.list.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User save(User user) {
+        User save = userRepository.save(user);
+        return save;
+    }
+}
