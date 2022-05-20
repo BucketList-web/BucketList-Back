ddbc89dcf9642bb5a4431e79dc52affbf337a35b
Daeyeon Ko <dyko615@naver.com>
2022-05-20 22:19:55 +0900

    댓글 리포지토리

--- src/main/java/bucket/list/service/UserService.java
+++ src/main/java/bucket/list/service/UserService.java
@@ -5,6 +5,8 @@ import bucket.list.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -21,8 +23,27 @@ public class UserService {
         return save;
     }
 
-    public String exist(String user_id) {
 
-        if(user)
+    public boolean exist(String user_id) {
+
+        User user = userRepository.exist(user_id);
+
+//        User user = new User();
+
+        if (user.getUser_phone() == null) {
+
+//            throw new IllegalStateException("이미 존재하는 ID 입니다");
+
+//            user.setUser_exist(false);
+            return true;
+
+        } else {
+//            user.setUser_exist(true);
+
+//            return user.isUser_exist();
+
+            return false;
+
+        }
     }
 }
