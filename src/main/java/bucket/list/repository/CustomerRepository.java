a4ff936688a214aba1fedadbbafaebe19f9bb101
Daeyeon Ko <dyko615@naver.com>
2022-05-24 20:43:10 +0900

    customer 리포지토리입니다

---
+++ src/main/java/bucket/list/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package bucket.list.repository;
+
+import bucket.list.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
