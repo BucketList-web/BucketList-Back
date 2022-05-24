514b89d583ceae536a1ace72fe387f1bbba34c0e
Daeyeon Ko <dyko615@naver.com>
2022-05-24 20:42:45 +0900

    parti 도메인입니다

--- src/main/java/bucket/list/domain/Participation.java
+++ src/main/java/bucket/list/domain/Participation.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -15,12 +17,27 @@ public class Participation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "participationidx")
     private Integer participationidx;
+
     private String participation_option;
+
+//    @NotBlank(message = "태그를 작성해주세요")
     private String participation_tag;
+
+//    @NotBlank(message = "장소를 작성해주세요")
     private String participation_place;
-    private int participation_cost;
-    private int participation_party;
+
+
+    @Column(columnDefinition = "integer default 0")
+//    @NotNull(message = "비용을 입력해주세요")
+    private Integer participation_cost;
+
+    @Column(columnDefinition = "integer default 0")
+//    @NotNull(message = "인원수를 입력해주세요")
+    private Integer participation_party;
+
+//    @NotBlank(message = "게시글의 내용을 적어주세요")
     private String participation_text;
+
     private String participation_file;
     private LocalDate participation_date;
     private String participation_writer;
